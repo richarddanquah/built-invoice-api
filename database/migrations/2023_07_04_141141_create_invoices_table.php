0bb07c929b8af5da209f831b147e1df8e7c0a656
Richard  Danquah <richpee60@yahoo.com>
2023-07-04 14:18:02 +0000

    schema creation

---
+++ database/migrations/2023_07_04_141141_create_invoices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->float('amount');
+            $table->string('status')->default('Billed');
+            $table->dateTime('issue_date');
+            $table->dateTime('due_date');
+            $table->dateTime('paid_date')->nullable();
+            $table->string('payment_method')->default('Cash');
+            $table->string('payment_ref');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
